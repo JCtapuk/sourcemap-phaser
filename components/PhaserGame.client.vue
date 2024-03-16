3efcd9384740974af704eb395af4f8c07e8c1673
JCtapuk <61321608+JCtapuk@users.noreply.github.com>
2024-03-16 16:25:26 +0000

    init

---
+++ components/PhaserGame.client.vue
@@ -0,0 +1,17 @@
+<template>
+    <div ref="parent" />
+  </template>
+  
+  <script setup lang="ts">
+  import type Phaser from "phaser";
+  
+  const parent = useState<HTMLElement>("phaser.parent");
+  
+  onMounted(() => {
+    import("phaser").then(({ Game }) => {
+      const game = new Game({
+        parent: parent.value,
+      });
+    });
+  });
+  </script>
